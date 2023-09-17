48d39476ebc630b576ca63cd46ad4c4bff6ae0e0
unknown <hoangkhoa204@yahoo.com>
2023-09-16 22:53:26 -0700

    restructured commands

--- pymeasure/instruments/siglenttechnologies/testfile.py
+++ pymeasure/instruments/siglenttechnologies/testfile.py
@@ -1,6 +1,6 @@
 from pymeasure.instruments.instrument import Instrument
 import pyvisa
-from pymeasure.instruments.validators import strict_discrete_set
+from pymeasure.instruments.validators import strict_discrete_set, custom_voltage_range_validator
 from pymeasure.adapters import VISAAdapter
 
 
@@ -34,13 +34,140 @@ class SDLbase(Instrument):
         dynamic=True
     )
 
+    event_status = Instrument.control(
+        "*ESE?", "*ESE %d",
+        """ 
+        Query the standard event status enable register.
+        Returns:
+        int: The value returned reflects the current state of all the bits in the register..
+        """,
+        dynamic=True
+    )
+
+    status_byte_enable = Instrument.control(
+        "*SRE?", "*SRE %i",
+        """
+        Query the status byte enable register. 
+
+        Returns:
+            int: The value in the register.
+        """,
+        dynamic=True
+
+    )
+
+    mode_transient_operation = Instrument.control(
+        ':SOURce:FUNCtion:TRANsient?', ':SOURce:FUNCtion:TRANsient %s',
+        """
+        get mode in transient operation (CC/CV/CP/CR).
+
+        Returns:
+            mode (str): The mode to set ("CURRent", "VOLTage", "POWer", or "RESistance").
+        """,
+        validator=strict_discrete_set,
+        values=["CURRent", "VOLTage", "POWer", "RESistance"],
+        dynamic=True
+
+    )
+
+    mode_static_operation = Instrument.control(
+        ":SOURce:FUNCtion?", ":SOURce:FUNCtion %s",
+        """
+        Sets mode in static operation (CC/CV/CP/CR/LED)
+
+        Args:
+            mode (str): The mode to set ("CURRent", "VOLTage", "POWer", "RESistance", or "LED").        
+        """,
+        validator=strict_discrete_set,
+        values=["CURRent", "VOLTage", "POWer", "RESistance", "LED"],
+        dynamic=True
+    )
+
+    current_range_CC_static_operation = Instrument.control(
+        ":SOURce:CURRent:IRANGe?", ":SOURce:CURRent:IRANGe %s",
+        """
+        Set the current range of CC mode in static operation.
+
+        Args:
+            value (str): The current range value to set. It can be any setting value, "MINimum", "MAXimum", or "DEFault".
+                         If <value> is greater than 5, the current range will be set to 30A; if <value> is less than 5,
+                         the current range will be set to 5A.
+        """,
+        validator=custom_voltage_range_validator,
+        values=["MINimum", "MAXimum", "DEFault"] ,
+        dynamic=True
+    )
+
+    voltage_range_CC_static_operation = Instrument.control(
+        ':SOURce:CURRent:VRANGe?', ':SOURce:CURRent:VRANGe %s',
+        """
+        Set the voltage range of CC mode in static operation.
+
+        Args:
+            value (str): The voltage range value to set. It can be any setting value, "MINimum", "MAXimum", or "DEFault".
+                         If <value> is greater than 36, the voltage range will be set to 150V; if <value> is less than 36,
+                         the voltage range will be set to 36V.
+        """,
+        validator=custom_voltage_range_validator,
+        values=["MINimum", "MAXimum", "DEFault"],
+        dynamic=True
+    )
+
+    voltage_value_CV_static_operation = Instrument.control(
+        ':SOURce:VOLTage:LEVel:IMMediate?', 'SOURce:VOLTage:LEVel:IMMediate %s',
+        """
+        Sets the preset voltage value of CV mode in static operation.
+
+        Args:
+            value (str): The voltage level to set (default is "DEFault").
+        """,
+        validator=custom_voltage_range_validator,
+        values=["MINimum", "MAXimum", "DEFault"],
+        dynamic=True
+    )
+
+    time_measurement_switch = Instrument.control(
+        'TIME:TEST:STATe?', 'TIME:TEST:STATe %s',
+        """
+        Sets whether enable the time measurement switch.
+
+        Args:
+            state (str): The state to set ("ON", "OFF", "1", or "0").
+        """,
+        validator=strict_discrete_set,
+        values=["ON", "OFF", "1",  "0"] ,
+        dynamic=True
+
+    )
+
     def getIDN(self):
         self.write("*IDN?")
         return self.adapter.read()
 
-print(instrument, adapter)
     
 siglentObject = SDLbase(adapter)
+
+print(siglentObject.event_status)
+siglentObject.event_status = -1
+print(siglentObject.event_status)
+siglentObject.status_byte_enable = 15
+print(siglentObject.status_byte_enable)
+siglentObject.mode_static_operation = "CURRent"
+print(siglentObject.mode_static_operation)
+
+siglentObject.current_range_CC_static_operation = "DEFault"
+print(siglentObject.current_range_CC_static_operation)
+
+siglentObject.voltage_range_CC_static_operation = "MINimum"
+print(siglentObject.voltage_range_CC_static_operation)
+
+siglentObject.voltage_value_CV_static_operation = "MINimum"
+print(siglentObject.voltage_value_CV_static_operation)
+
+siglentObject.time_measurement_switch = "OFF"
+print(siglentObject.time_measurement_switch)
+# siglentObject.mode_transient_operation = "CURRent"
+# print(siglentObject.mode_transient_operation)
 print(siglentObject.getIDN())
 # siglentObject.get_input_status()
 # siglentObject.source_input
